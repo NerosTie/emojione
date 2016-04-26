9747f635861eca2fdfacbe95fb454757d2b48a89
Neros <contact@neros.fr>
2016-04-26 15:16:30 +0200

    Create emojione.js

---
+++ emojione/emojione.js
@@ -0,0 +1,8 @@
+var posts = document.querySelectorAll('.link-tag, .link-description, .link-title, a, p, .dailyEntryTags');
+emojione.imageType = 'svg';
+emojione.imagePathSVG = "https://cdn.jsdelivr.net/emojione/assets/svg/";
+emojione.imagePathPNG = "https://cdn.jsdelivr.net/emojione/assets/png/";
+Array.prototype.forEach.call(posts, function(el, i){
+  var parsed = emojione.toImage(el.innerHTML);
+  el.innerHTML = parsed;
+});
