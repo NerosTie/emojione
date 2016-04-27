844534cd3440a407c3b804ca08aca6d7c9409ea0
Neros <contact@neros.fr>
2016-04-27 11:49:35 +0200

    Update emojione.php

--- emojione.php
+++ emojione.php
@@ -8,12 +8,12 @@ function hook_emojione_render_includes($data)
 
 function hook_emojione_render_footer($data)
 {
-    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.js';
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.min.js';
+    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.js';
 
     /* Uncomment the three lines below to use the autocomplete function */
 //    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/zepto.min.js';
-//    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/jquery.textcomplete.min.js';
+//    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/textcomplete.min.js';
 //    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/autocomplete.js';
 
     return $data;
