0d26cd76366980b8a100e523ce38f6c27accd3dd
Neros <contact@neros.fr>
2016-04-27 12:16:12 +0200

    Update emojione.php

--- emojione.php
+++ emojione.php
@@ -11,10 +11,13 @@ function hook_emojione_render_footer($data)
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.min.js';
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.js';
 
-    /* Uncomment the three lines below to use the autocomplete function */
-//    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/zepto.min.js';
-//    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/textcomplete.min.js';
-//    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/autocomplete.js';
+/* 
+* Uncomment the three lines below to use the autocomplete function.
+* If your theme use jquery, let the first line commented.
+*/
+#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/zepto.min.js';
+#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/textcomplete.min.js';
+#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/autocomplete.js';
 
     return $data;
 }
