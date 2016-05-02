12d2cce716aa418a535bc0427e65d1469ec7b043
kalvn <kalvnthereal@gmail.com>
2016-05-02 22:34:47 +0200

    Only loads autocomplete resources when on editlink page

--- emojione.php
+++ emojione.php
@@ -2,23 +2,29 @@
 function hook_emojione_render_includes($data)
 {
     $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/css/emojione.css';
-    $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/css/autocomplete.css';
+
+    if($data['_PAGE_'] === Router::$PAGE_EDITLINK){
+        $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/css/autocomplete.css';
+    }
+
     return $data;
 }
 
 function hook_emojione_render_footer($data)
 {
+    /*
+     * Uncomment the three lines below to use the autocomplete function.
+     * If your theme use jquery, let the first line commented.
+     */
+#    if($data['_PAGE_'] === Router::$PAGE_EDITLINK){
+#        $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/jquery.min.js';
+#        $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/textcomplete.min.js';
+#        $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/autocomplete.js';
+#    }
+
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.min.js';
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/unicode.js';
     $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/emojione.js';
-
-/*
-* Uncomment the three lines below to use the autocomplete function.
-* If your theme use jquery, let the first line commented.
-*/
-#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/jquery.min.js';
-#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/textcomplete.min.js';
-#    $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/emojione/js/autocomplete.js';
 
     return $data;
 }
