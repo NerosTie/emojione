a4d66ffcbf5d595424ab3e77d82a1a875a56c3f0
NerosTie <contact@neros.fr>
2016-07-16 22:16:04 +0200

    tab fix

--- assets/js/emojione.js
+++ assets/js/emojione.js
@@ -4,6 +4,6 @@ emojione.imageType = 'svg'; /* change to 'png' to have emojis in PNG */
 emojione.imagePathSVG = "../plugins/emojione/assets/svg/";
 emojione.imagePathPNG = "../plugins/emojione/assets/png/";
 Array.prototype.forEach.call(posts, function(el, i){
-  var parsed = emojione.toImage(el.innerHTML);
-  el.innerHTML = parsed;
+    var parsed = emojione.toImage(el.innerHTML);
+    el.innerHTML = parsed;
 });
--- assets/js/unicode.js
+++ assets/js/unicode.js
@@ -1,5 +1,5 @@
 var posts = document.querySelectorAll('.link-tag, .link-description, .link-title, a, p, .dailyEntryTags');
 Array.prototype.forEach.call(posts, function(el, i){
-  var parsed = emojione.shortnameToUnicode(el.innerHTML);
-  el.innerHTML = parsed;
+    var parsed = emojione.shortnameToUnicode(el.innerHTML);
+    el.innerHTML = parsed;
 });
